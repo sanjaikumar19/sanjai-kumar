820c82865388f9b27dc4a93fd8f6aa5ddb60dcb6
sanjaikumar19 <58897220+sanjaikumar19@users.noreply.github.com>
2019-12-22 14:28:57 +0530

    Add files via upload

---
+++ submitservlet.java
@@ -0,0 +1,57 @@
+package project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class submitservelet
+ */
+@WebServlet("/submitservelet")
+public class submitservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public submitservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+       String user=request.getParameter("uname");
+       String pass =request.getParameter("pwd");
+       if(user.contentEquals("sanjai")&&pass.contentEquals("pass@123"))
+       {
+    	   RequestDispatcher dis= getServletContext().getRequestDispatcher("/WelcomeServelet");
+    	   dis.forward(request, response);
+       }
+       else
+       {
+    	   RequestDispatcher dis= getServletContext().getRequestDispatcher("/HomeServelet");
+    	   dis.include(request, response);
+    	   PrintWriter out=response.getWriter();
+    	   out.println("invalid login");
+       }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
